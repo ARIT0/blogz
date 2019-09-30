8c3cd77a896b6cd3cd31f096ed14203633629cc0
Ariadna Pulido <adna.pulido@gmail.com>
2019-09-30 13:08:58 -0400

    created online database, error messages, and improved format

--- main.py
+++ main.py
@@ -12,35 +12,54 @@ db = SQLAlchemy(app)
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(120))
+    body = db.Column(db.Text())
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
 
-#titles = []
-posts = []
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
 
     if request.method == 'POST':
-        entry_title = request.form['entry_title']
+        entry_title = request.form["entry_title"]
         blog_entry = request.form["blog_entry"]
-        #titles.append(entry_title)
-        posts.append(entry_title)
-        posts.append(blog_entry)
 
-        return redirect('/blog')
+        title_error = ""
+        entry_error = ""
+
+        if not entry_title:
+            title_error= "Please enter a title"
+            return render_template('add_new_post.html', blog_entry= blog_entry, title_error=title_error)
+        if not blog_entry:
+            entry_error = "Please enter a blog post"
+            return render_template('add_new_post.html', entry_title=entry_title, entry_error=entry_error)
+        else:
+            new_post = Blog(entry_title, blog_entry)
+            db.session.add(new_post)
+            db.session.commit()
+
+            #blog_id = new_post.id
+            #blog = Blog.query.filter_by(id = blog_id).first()
+
+            return redirect('/blog')
     
     return render_template('add_new_post.html')
     
 
 @app.route('/blog')  #displays all blog posts
-def blogs():
-    return render_template('main_blog_page.html', posts=posts)
+def blog():
+    blog = Blog.query.all()
 
-#@app.route('/newpost', ['POST'])
+    return render_template('main_blog_page.html', blog=blog)
+
+
+@app.route('/')
+def index():
+    return redirect('/newpost')
+
 
 if __name__ == '__main__':
     app.run()
